e04fc148626f850a92cfe9fde5a4cbed98f0f01c
Paul Sweeney <paul@8-bitdesign.com>
2020-09-27 22:54:41 -0700

    Add momentum command

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const token = process.env.TOKEN
+const prefix = '!'
+
 const handleRoll = require('./lib/roll-command.js')
+const handleMomentum = require('./lib/momentum.js')
 
 client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`) })
-client.on('message', handleRoll)
+
+client.on('message', (msg) => {
+  if (msg.author.bot) return
+  if (!msg.content.startsWith(prefix)) return
+
+  const text = msg.content.slice(prefix.length)
+  const [command, ...args] = text.split(' ')
+
+  switch (command) {
+    case 'roll': return handleRoll(args, msg)
+    case 'momentum': return handleMomentum(args, msg)
+    default: return msg.reply(`I'm afraid I don't know how to \`${command}\``)
+  }
+})
+
 client.login(token)
