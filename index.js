b5b3f63bc246c6912def031c6ad1bf7954c25331
Paul Sweeney <paul@8-bitdesign.com>
2020-10-29 13:58:20 -0700

    Updating help text

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ const commands = {
 
 function help (command, msg) {
   if (command && command.usage) {
-    msg.reply(`*${command.name}*: ${command.description}\n${command.usage}`)
+    msg.reply(`**${command.name}**: ${command.description}\n${command.usage}`)
   } else {
-    const response = ['I know the following commands:']
+    const response = ['I know the following commands:\n']
 
     if (command) {
       response.unshift(`I'm afraid I don't know how to \`${command}\`.`)
     }
 
     Object.values(commands).forEach(cmd => {
-      response.push(`- *${cmd.name}*: ${cmd.description}`)
+      response.push(`- **${cmd.name}**: ${cmd.description}`)
     })
 
-    msg.reply(response.concat('Use `!help [command]` to learn more.').join('\n'))
+    msg.reply(response.concat('\nUse `!help [command]` to learn more.').join('\n'))
   }
 }
 
