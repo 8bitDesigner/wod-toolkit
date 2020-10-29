e8a56dc2801887741fc25bd8422a62cf960689e3
Paul Sweeney <paul@8-bitdesign.com>
2020-10-29 14:01:44 -0700

    Updating help text

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ function help (command, msg) {
   }
 }
 
-client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`) })
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
 client.on('message', (msg) => {
   if (msg.author.bot) return
